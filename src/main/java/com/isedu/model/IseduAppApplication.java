56319ff1b24c222d688be6e45a75f4ac9336a73d
durgamatha@6 <sumansukumar26@yahoo.com>
2020-07-22 18:00:39 +0530

    first commit

---
+++ src/main/java/com/isedu/model/IseduAppApplication.java
@@ -0,0 +1,13 @@
+package com.isedu.model;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IseduAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IseduAppApplication.class, args);
+	}
+
+}
